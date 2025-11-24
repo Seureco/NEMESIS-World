9dc73f88bf7af006456af9a0fccff8a6a21bc3d5
bapboitier <baptiste.boitier@erasme-team.eu>
2025-11-24 11:52:20 +0100

    Mettre à jour conf.py, index.rst et requirements.txt pour RTD

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -9,12 +9,15 @@
 project = 'NEMESIS-World'
 copyright = '2025, Baptiste Boitier (SEURECO), Pierre Le Mouël (SEURECO), Paul Zagamé (SEURECO), Hayat Mekki (SEURECO)'
 author = 'Baptiste Boitier (SEURECO), Pierre Le Mouël (SEURECO), Paul Zagamé (SEURECO), Hayat Mekki (SEURECO)'
-release = '1.0.0.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',  
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
